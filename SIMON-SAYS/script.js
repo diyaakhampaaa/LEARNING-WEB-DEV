8aa6c7b23f42d5925fab96d6f3eae100f4b9b7b8
DIYA KHAMPA <khampadiya8@gmail.com>
2026-06-24 10:23:39 +0530

    Add files via upload

---
+++ SIMON-SAYS/script.js
@@ -0,0 +1,157 @@
+const colors = ["green", "red", "yellow", "blue"];
+
+let gamePattern = [];
+let userPattern = [];
+
+let level = 0;
+let score = 0;
+
+let started = false;
+
+const startBtn = document.getElementById("start-btn");
+const levelText = document.getElementById("level");
+const scoreText = document.getElementById("score");
+const message = document.getElementById("message");
+const buttons = document.querySelectorAll(".color-btn");
+
+// Start Game
+startBtn.addEventListener("click", startGame);
+
+function startGame() {
+
+    if (started) return;
+
+    started = true;
+
+    level = 0;
+    score = 0;
+
+    gamePattern = [];
+    userPattern = [];
+
+    message.textContent = "Watch the sequence!";
+
+    nextLevel();
+}
+
+// Next Level
+function nextLevel() {
+
+    userPattern = [];
+
+    level++;
+
+    levelText.textContent = `Level ${level}`;
+    scoreText.textContent = `Score ${score}`;
+
+    const randomColor =
+        colors[Math.floor(Math.random() * 4)];
+
+    gamePattern.push(randomColor);
+
+    playSequence();
+}
+
+// Flash Button
+function flashButton(color) {
+
+    const button =
+        document.querySelector(`.${color}`);
+
+    button.classList.add("active");
+
+    setTimeout(() => {
+
+        button.classList.remove("active");
+
+    }, 400);
+}
+
+// Play Sequence
+function playSequence() {
+
+    let i = 0;
+
+    const interval = setInterval(() => {
+
+        flashButton(gamePattern[i]);
+
+        i++;
+
+        if (i >= gamePattern.length) {
+
+            clearInterval(interval);
+
+        }
+
+    }, 800);
+}
+
+// User Clicks
+buttons.forEach(button => {
+
+    button.addEventListener("click", handleUserClick);
+
+});
+
+function handleUserClick() {
+
+    if (!started) return;
+
+    const color = this.dataset.color;
+
+    userPattern.push(color);
+
+    flashButton(color);
+
+    checkAnswer();
+}
+
+// Check Answer
+function checkAnswer() {
+
+    const currentIndex =
+        userPattern.length - 1;
+
+    if (
+        userPattern[currentIndex] !==
+        gamePattern[currentIndex]
+    ) {
+
+        gameOver();
+        return;
+    }
+
+    if (
+        userPattern.length ===
+        gamePattern.length
+    ) {
+
+        score += 10;
+
+        scoreText.textContent =
+            `Score ${score}`;
+
+        setTimeout(() => {
+
+            nextLevel();
+
+        }, 1000);
+    }
+}
+
+// Game Over
+function gameOver() {
+
+    message.textContent =
+        `Game Over! Final Score: ${score}`;
+
+    levelText.textContent = "Level 0";
+
+    started = false;
+
+    gamePattern = [];
+    userPattern = [];
+
+    level = 0;
+}
